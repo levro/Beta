f4fd26f974f3e145eb22b2ef19c4357fba74ec5e
bios <bios@ukr.net>
2018-10-19 23:17:03 +0300

    log4j + logfile

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 
 import java.lang.*;
 
-
 public class Main {
     final static Logger log = LogManager.getLogger( Main.class);
     public static void main(String[] args) {
