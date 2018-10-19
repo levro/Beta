28bfe40c84c0e8efe895cd52489c7f031ee7c4dc
bios <bios@ukr.net>
2018-10-20 00:05:11 +0300

    log4j custom levels

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,11 +1,13 @@
 package com.example;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.lang.*;
 
 public class Main {
+    static final Level epicFail = Level.forName("EPIC.FAIL!", 50);
     final static Logger log = LogManager.getLogger( Main.class);
     public static void main(String[] args) {
         log.debug( "==================MAIN.START==================");
@@ -16,6 +18,7 @@ public class Main {
         log.warn("This is a warn message");
         log.error("This is an error message");
         log.fatal("This is a fatal message");
+        log.log(epicFail, "This is a epicFail message");
         System.out.println("\nHello Siddharth\n");
         log.debug( "main end");
     }
